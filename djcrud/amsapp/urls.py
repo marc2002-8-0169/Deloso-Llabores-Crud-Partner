8f43db1fddd3e4b9fa52502aa0764400feceabca
Marc Deloso <202080169@psu.palawan.edu.ph>
2023-06-25 18:17:56 +0800

    url-main

--- djcrud/amsapp/urls.py
+++ djcrud/amsapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='main'),
+    path('main/', views.main, name='main'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('add/', views.add, name='add'),
