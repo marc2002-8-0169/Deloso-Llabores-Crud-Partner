b73023357c9bf9af009181284c3f970162ba4e3f
Marc Deloso <202080169@psu.palawan.edu.ph>
2023-06-25 21:26:37 +0800

    txtFile

--- djcrud/amsapp/urls.py
+++ djcrud/amsapp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('add/', views.add, name='add'),
+    path('addstudent/', views.addstudent, name='addstudent'),
     path('delete/<int:assignment_id>', views.delete, name='delete'),
     path('update/<int:assignment_id>', views.update, name='update'),
     path('studentupdate/<int:student_id>', views.studentupdate, name='studentupdate'),
